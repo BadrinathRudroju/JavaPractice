2ec2d5c56ccde600c47c5dd196cb6e30115efa4b
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-04-08 18:20:03 +0530

    Linear search practice

---
+++ src/SearchInStrings.java
@@ -0,0 +1,18 @@
+public class SearchInStrings {
+    public static void main(String[] args){
+        String name = "Badri";
+        char target = 'r';
+        System.out.println(searchString(name,target));
+    }
+    static boolean searchString(String name, char target){
+        if(name.length() == 0){
+            return false;
+        }
+        for(int i = 0; i< name.length(); i++){
+            if(name.charAt(i) == target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
