b28b1632bbc1374850f9dfc8339cd3a059eb62ef
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-04-09 22:40:23 +0530

    Linear search practice

--- src/SearchInRange.java
+++ src/SearchInRange.java
@@ -1,12 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class SearchInRange {
     public static void main(String[] args){
-        int[] arr = {3,4,5,6,7,7,5};
-        int target = 5;
-        System.out.println(searchinrange(arr,target,2,5));
+       Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int size = scanner.nextInt();
+       int[] arr = new int[size];
+        System.out.println("Enter elements of array: ");
+        try {
+            for (int i = 0; i < size; i++) {
+                arr[i] = scanner.nextInt();
+            }
+        }catch(InputMismatchException e){
+            System.out.println("Recheck data type");
+            scanner.close();
+            return;
+        }
+        System.out.print("Element to found: ");
+       int target = scanner.nextInt();
+        System.out.println("Enter range: ");
+        System.out.print("Start: ");
+        int start = scanner.nextInt();
+        System.out.print("End: ");
+        int end = scanner.nextInt();
+
+        System.out.println(searchinrange(arr,target,start,end));
     }
 
     static String searchinrange(int[] arr, int target, int start, int end){
-        if(arr.length == 0){
+        if(arr.length == 0 || start<0 || end>arr.length || start>end ){
             return "Does not exist";
         }
         for(int i=start; i<= end; i++){
