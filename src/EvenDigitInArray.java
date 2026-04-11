f21ae9680620b4848d80b13731a857ab40cdc6aa
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-04-11 15:00:31 +0530

    Linear search practice

---
+++ src/EvenDigitInArray.java
@@ -0,0 +1,29 @@
+public class EvenDigitInArray {
+    public static void main(String[] args){
+        int[] nums ={12,345,2,6,-7896};
+        System.out.println(findnumbers(nums));
+    }
+    static int findnumbers(int[] nums){
+        int count=0;
+        for(int i=0; i< nums.length; i++){
+            if(even(nums[i])){
+            count++;
+            }
+        }
+        return count;
+    }
+    static boolean even(int num){
+        return checkdigits(num) % 2 == 0;
+    }
+    static int checkdigits(int num){
+        if(num < 0){
+            num = num * -1;
+        }
+        int count =0;
+        while(num > 0){
+            count++;
+            num = num /10;
+        }
+        return count;
+    }
+}
