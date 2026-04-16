f21d013a8a7703db2446ee94d964b42185a830d1
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-04-16 11:50:05 +0530

    Fibonacci

---
+++ src/CoreJava/fibonacci.java
@@ -0,0 +1,15 @@
+package CoreJava;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 10;
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print( a +" ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+    }
+}
+
