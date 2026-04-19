0a371821d66f7162ca02bbd729248f313dae8579
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-04-19 14:10:39 +0530

    palindrome

--- src/CoreJava/ispalindrome.java
+++ src/CoreJava/ispalindrome.java
@@ -1,15 +1,26 @@
 package CoreJava;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ispalindrome {
-  public static void main(String[] args){
+  public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
-      System.out.println("Enter word to confirm if it is palindrome");
-      String word = scanner.nextLine();
-      System.out.println(pal(word));
-      scanner.close();
-}
+      System.out.println("Enter digit or word to confirm if it is palindrome");
+      try {
+          if(scanner.hasNextInt()) {
+              int n = scanner.nextInt();
+              System.out.println(palnum(n));
+              scanner.close();
+          }else{
+              String name = scanner.nextLine();
+              System.out.println(pal(name));
+          }
+          scanner.close();
+      } catch (InputMismatchException e) {
+          System.out.println("Please enter valid option");
+      }
+  }
 public static boolean pal(String word){
      word = word.toLowerCase();
      for(int i=0; i<word.length()/2;i++){
@@ -20,6 +31,20 @@ public static boolean pal(String word){
           }
      }
      return true;
+   }
+
+   public static boolean palnum(int n){
+      int original = n;
+      int reversed =0;
+      if(n<0){
+          return false;
+      }
+      while(n>0){
+          int rem = n % 10;
+          reversed = reversed * 10 + rem;
+          n = n /10;
+      }
+     return original == reversed;
    }
 }
 
