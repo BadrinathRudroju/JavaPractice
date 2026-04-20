3eba4b2edc10c2eaad2b79f53f9f95422ceae63a
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-04-20 14:54:01 +0530

    Armstrong

---
+++ src/CoreJava/IsArmstrong.java
@@ -0,0 +1,17 @@
+package CoreJava;
+
+public class IsArmstrong {
+    public static void main(String[] args){
+        System.out.println(arm(153));
+    }
+    static boolean arm(int num){
+        int original = num;
+        int sum =0;
+        while(num > 0) {
+            int digit = num % 10;
+            sum = sum + digit * digit * digit;
+            num = num / 10;
+        }
+        return sum == original;
+    }
+}
